8f15ed044aaea1bc848daba632062a0735121ecb
Falk Werner <falk.werner@gmx.net>
2023-12-20 00:13:15 +0100

    backend: allow to read and write tags

--- src/preload.js
+++ src/preload.js
@@ -6,5 +6,7 @@ contextBridge.exposeInMainWorld('note', {
     create: () => ipcRenderer.invoke('createNote'),
     rename: (old_name, new_name) => ipcRenderer.invoke('renameNote', old_name, new_name),
     write: (name, contents) => ipcRenderer.invoke('writeNote', name, contents),
-    remove: (name) => ipcRenderer.invoke('removeNote', name)
+    remove: (name) => ipcRenderer.invoke('removeNote', name),
+    read_tags: (name) => ipcRenderer.invoke('readTags', name),
+    write_tags: (name, tags) => ipcRenderer.invoke('writeTags', name, tags)
 });
